e535272ca60fc32ae7e7ad0fc68eefad1df975db
jim <jim@laptop.(none)>
2010-07-17 09:26:52 -0500

    Work in progress

--- src/net/intensivesystems/conduit-rabbitmq.clj
+++ src/net/intensivesystems/conduit-rabbitmq.clj
@@ -4,10 +4,11 @@
      [clojure.pprint :only [pprint]]
      [net.intensivesystems.conduit :only [conduit conduit-run new-proc
                                           new-id seq-fn seq-proc a-run
-                                          scatter-gather-fn proc-fn-future
+                                          scatter-gather-fn
                                           reply-proc]]
      net.intensivesystems.arrows)
   (:import
+     [java.util UUID]
      [com.rabbitmq.client Connection ConnectionFactory Channel
       MessageProperties QueueingConsumer]))
 
@@ -24,6 +25,7 @@
 (defn publish [queue msg]
   (let [msg-str (binding [*print-dup* true]
                   (pr-str msg))]
+    (println "msg-str:" msg-str)
     (.basicPublish *channel* *exchange* queue
                    (MessageProperties/PERSISTENT_TEXT_PLAIN)
                    (.getBytes msg-str))))
@@ -38,19 +40,25 @@
     (.basicConsume *channel* queue, false, consumer)
     consumer))
 
+(defn get-message 
+  ([consumer]
+   (try
+     (.nextDelivery consumer)
+     (catch InterruptedException e
+       nil)))
+  ([consumer msecs]
+   (.nextDelivery consumer msecs)))
+
 (defn msg-list 
   ([consumer]
-   (if-let [next-msg (try
-                       (.nextDelivery consumer)
-                       (catch InterruptedException e
-                         nil))]
+   (if-let [next-msg (get-message consumer)]
      (lazy-seq
-       (cons [(read-string (String. (.getBody next-msg)))
+       (cons [
               next-msg]
              (msg-list consumer)))
      (msg-list consumer)))
   ([consumer msecs]
-   (when-let [next-msg (.nextDelivery consumer msecs)]
+   (when-let [next-msg (get-message consumer msecs)]
      (lazy-seq
        (cons [(read-string (String. (.getBody next-msg)))
               next-msg]
@@ -59,6 +67,16 @@
 (defn rabbit-msgs
   ([queue] (msg-list (consumer queue)))
   ([queue msecs] (msg-list (consumer queue) msecs)))
+
+(defn rabbit-msg
+  ([queue]
+   (let [next-msg (get-message (consumer queue))]
+     [(read-string (String. (.getBody next-msg)))
+      next-msg]))
+  ([queue msecs]
+   (let [next-msg (get-message (consumer queue) msecs)]
+     [(read-string (String. (.getBody next-msg)))
+      next-msg])))
 
 (with-arrow conduit
   (defn rabbitmq-handler [p queue]
@@ -75,9 +93,23 @@
           (a-run
             (a-seq (a-nth 0 (rabbitmq-handler p queue))
                    (a-nth 1 (a-arr ack-message)))
-            (rabbit-msgs queue 100)))))))
+            (rabbit-msgs queue 100))))))
+            
+  (defn rabbitmq-arr [source f]
+    (let [id (new-id)
+          proc-fn (fn this-fn [x]
+                    (println "x:" x)
+                    [[(f x)] this-fn])
+          #_(:fn (a-arr f))
+          source (str source)]
+      {:type :rabbitmq
+       :fn proc-fn
+       :parts {source {:type :rabbitmq
+                             id proc-fn}}
+       :source source
+       :id id})))
 
-(defmacro def-rabbitmq [proc-name source old-proc]
+#_(defmacro def-rabbitmq [proc-name source old-proc]
   `(def ~proc-name {:type :rabbitmq
                     :fn (:fn ~old-proc)
                     :parts {(str ~source) {:type :rabbitmq
@@ -109,20 +141,24 @@
            :fn new-fn
            :parts new-parts)))
 
-#_(defmethod scatter-gather-fn :rabbitmq [p]
-  (fn [x]
-    create-reply-queue
-    (publish (:source p2) [x reply-queue])
-    return future that waits on reply
-    ))
+(defmethod scatter-gather-fn :rabbitmq [p]
+  (fn this-fn [x]
+    (let [reply-queue (str (UUID/randomUUID))]
+      (.queueDeclare *channel* reply-queue false false false {})
+      (.queueBind *channel* reply-queue *exchange* reply-queue)
+      (println "scatter:" (:source p) [(:id p) [x reply-queue]]) (flush)
+      (publish (:source p) [(:id p) [x reply-queue]])
+      (fn []
+        (println "waiting on:" reply-queue)
+        [(rabbit-msg reply-queue) this-fn]))))
 
 (defmethod reply-proc :rabbitmq [p]
-  (new-proc p
-            (partial (fn this-fn [f [x reply-queue]]
-                       (let [[new-x new-f] (f x)]
-                         (publish reply-queue new-x)
-                         [[] (partial this-fn new-f)]))
-                     (:fn p))))
+  (with-arrow conduit
+              (a-seq (a-nth 0 p)
+                     {:fn (fn this-fn [[x reply-queue]]
+                            (println "response:" reply-queue x)
+                            (publish reply-queue x)
+                            [[] this-fn])})))
 
 (defn rabbitmq-connection [host vhost user password]
   "Create a simple rabbitmq connection."
@@ -133,10 +169,13 @@
       (.setUsername user)
       (.setPassword password))))
 
+(declare *connection*)
+(declare *queue*)
 (defn rabbitmq-test-fixture [f]
   (with-open [connection (rabbitmq-connection "localhost" "/" "guest" "guest")
               channel (.createChannel connection)]
-    (binding [*channel* channel
+    (binding [*connection* connection
+              *channel* channel
               *exchange* "conduit-exchange"
               *queue* "test-queue"]
       (.exchangeDeclare channel *exchange* "direct")
@@ -147,13 +186,12 @@
 (use-fixtures :each rabbitmq-test-fixture)
 
 (def test-results (atom []))
-(with-arrow conduit
-            (def-rabbitmq test-rabbit 'test-queue
-                          (a-arr (fn [n]
-                                   (swap! test-results conj n)
-                                   n))))
+#_(def test-rabbit (rabbitmq-arr 'test-queue
+                               (fn [n]
+                                 (swap! test-results conj n)
+                                 n)))
 
-(deftest test-rabbit-publish-consume
+#_(deftest test-rabbit-publish-consume
          (dorun
            (map (partial publish *queue*)
                 (range 50)))
@@ -164,10 +202,10 @@
                        s)
                      (rabbit-msgs *queue* 100)))))
 
-(deftest test-rabbitmq-run
+#_(deftest test-rabbitmq-run
          (dorun
            (map (partial publish *queue*)
-                (map (fn [x] ["test-rabbit" x])
+                (map (fn [x] [(:id test-rabbit) x])
                      (range 10))))
 
          (reset! test-results [])
@@ -175,7 +213,7 @@
          (is (= (range 10)
                 @test-results)))
 
-(deftest test-new-proc
+#_(deftest test-new-proc
         (let [new-rabbit (new-proc test-rabbit (fn this-fn [n]
                                                  (swap! test-results conj (inc n))
                                                  [[(inc n)] this-fn]))]
@@ -189,7 +227,7 @@
           (is (= (range 1 11)
                  @test-results))))
 
-(deftest test-seq-proc
+#_(deftest test-seq-proc
         (let [new-rabbit (with-arrow conduit
                                      (a-seq (a-arr inc)
                                             test-rabbit))]
@@ -199,5 +237,50 @@
           (conduit-run new-rabbit *queue* *channel* *exchange*)
           (is (= (range 1 11)
                  @test-results))))
+
+(defn bogus [f xs]
+  (println "first x:" (first xs))
+  (let [[new-x new-f] (f (ffirst xs))]
+    (if (empty? new-x)
+      (recur new-f ((second xs)))
+      (lazy-seq
+        (cons (first new-x)
+              (bogus new-f ((second xs))))))))
+
+(deftest test-par-proc
+         (let [new-rabbit (with-arrow conduit
+                                      (a-all (rabbitmq-arr "test-queue" inc)
+                                             (rabbitmq-arr "test-queue" dec)))
+               thread-fn (fn [exchange]
+                           (with-arrow conduit
+                                       (with-open [connection (rabbitmq-connection "localhost" "/" "guest" "guest")
+                                                   channel (.createChannel connection)]
+                                         (binding [*connection* connection
+                                                   *channel* channel
+                                                   *exchange* "conduit-exchange"]
+                                           (let [queue "test-queue"]
+                                             (dorun
+                                               (bogus
+                                                 (:fn (a-seq (a-nth 0 (rabbitmq-handler new-rabbit queue))
+                                                             (a-nth 1 (a-arr ack-message))))
+                                                 (rabbit-msgs queue 100))))))))
+              remote-thread (doto (new Thread (partial thread-fn *exchange*))
+                              (.start))
+              ]
+
+          (try
+            (a-run new-rabbit (range 10))
+            (reset! test-results [])
+            (is (= (map (fn [x y] [x y])
+                        (range 1 11)
+                        (range -1 9))
+                   @test-results))
+
+            (finally
+              (Thread/sleep 500)
+              (.interrupt remote-thread)
+              (println "waiting...")
+              (.join remote-thread 5000)
+              (println "done waiting")))))
 
 (run-tests)
