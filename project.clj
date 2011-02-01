43dedbaa2c85e30de450dd2075670b9f5c46c2fa
Jim Duey <jim@intensivesystems.net>
2011-02-01 13:46:53 -0600

    Moved to version 0.8.0

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject net.intensivesystems/conduit-rabbitmq "0.7.0-SNAPSHOT"
+(defproject net.intensivesystems/conduit-rabbitmq "0.8.0"
   :description "conduit-rabbitmq: 
                Build distributed applications in Clojure using RabbitMQ"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [com.rabbitmq/amqp-client "1.7.2"]
                  [net.intensivesystems/arrows "1.2.0"]
-                 [net.intensivesystems/conduit "0.7.0-SNAPSHOT"]])
+                 [net.intensivesystems/conduit "0.8.0"]])
